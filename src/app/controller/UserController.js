4e31684318561156e116983ee6474b67e00312d9
Ednaldo <martinsednaldo@outlook.com>
2020-09-10 15:12:32 -0300

    método update para atualizar os dados do usuário.

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -20,6 +20,10 @@ class UserController {
       },
     );
   }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
+import authMiddleware from './app/middleware/authMiddleware';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+//  rotas que exigem autenticacao
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
