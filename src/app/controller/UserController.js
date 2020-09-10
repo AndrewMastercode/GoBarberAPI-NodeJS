a4120a6de96fc5dabd259c20f711d7bac04c190a
Ednaldo <martinsednaldo@outlook.com>
2020-09-10 11:26:19 -0300

    Controlador para rotas de usuarios.

---
+++ src/app/controller/UserController.js
@@ -0,0 +1,25 @@
+import User from '../model/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne(
+      {
+        where: { email: req.body.email },
+      },
+    );
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exist.' });
+    }
+
+    const { name, email, provider } = await User.create(req.body);
+
+    return res.json(
+      {
+        name, email, provider,
+      },
+    );
+  }
+}
+
+export default new UserController();
