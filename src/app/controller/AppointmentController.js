7abe3a9da71bda36c4714d50a68efebf882e875a
Ednaldo <martinsednaldo@outlook.com>
2020-09-14 10:08:47 -0300

    Validação de data e disponibilidade.

--- src/app/controller/AppointmentController.js
+++ src/app/controller/AppointmentController.js
@@ -1,4 +1,6 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 import Appointment from '../model/Appointment';
 import User from '../model/User';
 
@@ -21,6 +23,24 @@ class AppointmentController {
 
     if (!isProvider) {
       return res.status(401).json({ error: 'You can only create appointments with providers' });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Appoitment date is not available' });
     }
 
     const appointment = await Appointment.create({
