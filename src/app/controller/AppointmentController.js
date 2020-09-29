474e1a24bf2079518bd6bc6403b11987b0a1d8d0
Ednaldo <martinsednaldo@outlook.com>
2020-09-29 10:14:15 -0300

    Movendo a checagem dos agendamento para um serviço a parte.

--- src/app/controller/AppointmentController.js
+++ src/app/controller/AppointmentController.js
@@ -1,13 +1,9 @@
-import {
-  startOfHour, parseISO, isBefore, format, subHours,
-} from 'date-fns';
-import pt from 'date-fns/locale/pt';
-
 import Appointment from '../model/Appointment';
 import User from '../model/User';
 import File from '../model/File';
-import Notification from '../schema/Notification';
-import Mail from '../../lib/Mail';
+
+import CreateAppointmentService from '../service/CreateAppointmentService';
+import DeleteAppointmentService from '../service/DeleteAppointmentService';
 
 class AppointmentController {
   async index(req, res) {
@@ -37,89 +33,17 @@ class AppointmentController {
   async store(req, res) {
     const { provider_id, date } = req.body;
 
-    const isProvider = await User.findOne({
-      where: { id: provider_id, provider: true },
-    });
-
-    if (!isProvider) {
-      return res.status(401).json({ error: 'You can only create appointments with providers' });
-    }
-
-    const hourStart = startOfHour(parseISO(date));
-
-    if (isBefore(hourStart, new Date())) {
-      return res.status(400).json({ error: 'Past dates are not permitted' });
-    }
-
-    const checkAvailability = await Appointment.findOne({
-      where: {
-        provider_id,
-        canceled_at: null,
-        date: hourStart,
-      },
-    });
-
-    if (checkAvailability) {
-      return res.status(400).json({ error: 'Appoitment date is not available' });
-    }
-
-    if (req.userId === provider_id) {
-      return res.status(400).json({ error: 'You cannot schedule with yourself' });
-    }
-
-    const appointment = await Appointment.create({
-      user_id: req.userId,
-      provider_id,
-      date,
-    });
-
-    //  notificacoes para o provedor
-    const user = await User.findByPk(req.userId);
-    const formatedDate = format(
-      hourStart,
-      "dd 'de' MMMM', às' H:mm'h'",
-      { locale: pt },
+    const appointment = await CreateAppointmentService.run(
+      { provider_id, user_id: req.userId, date },
     );
-
-    await Notification.create({
-      content: `Novo agendamento de ${user.name} para o dia ${formatedDate}`,
-      user: provider_id,
-    });
 
     return res.json(appointment);
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id, {
-      include: [{
-        model: User,
-        as: 'provider',
-        attributes: ['name', 'email'],
-      }],
-    });
-
-    if (appointment.user_id !== req.userId) {
-      return res.status(401).json({
-        error: 'You dont have permission to cancel this appointment',
-      });
-    }
-
-    const dateOffset = subHours(appointment.date, 2);
-
-    if (isBefore(dateOffset, new Date())) {
-      return res.status(401).json({
-        error: 'You can only cancel appointments 2 hours in advance',
-      });
-    }
-
-    appointment.canceled_at = new Date();
-
-    await appointment.save();
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      text: 'O seu agendamento foi cancelamento',
-    });
+    const appointment = await DeleteAppointmentService.run(
+      { user_id: req.userId, provider_id: req.params.id },
+    );
 
     return res.json(appointment);
   }
